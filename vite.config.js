ab8df6d57a407a10d48e0df801efb4e4e221dbd2
William Wang <bn.william.w@gmail.com>
2023-07-12 21:00:37 -0400

    Fix build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  build: {
+    rollupOptions: {
+      external: ["/assets/feature1.png","/assets/feature2.png","/assets/feature3.png"], // Add the file path here
+    },
+  },
 })
